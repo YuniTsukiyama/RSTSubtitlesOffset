9e93ec6ec70ef69340a37f22db2c321ac607bdd9
Arthur Hugonnot <arthur.hugonnot@epita.fr>
2021-06-07 02:32:21 +0200

    Add a working draft of the tool

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# RSTSubtitlesOffset
 Substract an offset to each subtitles of a .rst file
+
+Usage:
+$ chmod +x subtitles_offset.py
+$ ./subtitles_offset.py [RST FILE] [MINUTES] [SECONDS]
+
+This is a very primitive tool:
+- It does not remove subtitles when their timestamp underflow
+- It only takes minutes and second  as offset
+- It takes offset in separate parameters
+
+I've just quickly developed a dirty tool to adjust subtitles for videos having
+an offset between sound and subtitles so big that VLC was dying when I was
+setting the offset by hand.
---
+++ subtitles_offset.py
@@ -0,0 +1,64 @@
+#!/bin/python3
+
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+out = []
+
+# Return true if \c is a digit
+def is_digit(c):
+    int_c = ord(c)
+    return int_c >= 48 and int_c <= 57
+
+# Return true if \s is a number
+def is_num(s):
+    for c in s:
+        if (not(is_digit(c))):
+            return False
+    return True
+
+# Convert \date to a string with .rst format
+def to_str(date):
+    return "{0:02}:{1:02}:{2},{3:3.3}".format(date.hour,
+                                              date.minute,
+                                              date.second,
+                                              str(date.microsecond))
+
+with open(sys.argv[1], 'r', encoding='utf-8') as ifile:
+
+    # Split file in lines
+    lines = ifile.read().split('\n')
+
+    # Time offset
+    offset = timedelta(minutes=int(sys.argv[2]), seconds=int(sys.argv[3]))
+    # Index offset
+    offset_s = int(lines[0]) - 1;
+
+    for line in lines:
+        if (line[0:3] == "00:"):                    # Timestamps
+            newline = ""
+
+            # Add begin of subtitle
+            begin_str = line[0:line.find(' ')]
+            begin = datetime.strptime(begin_str, "%H:%M:%S,%f")
+            newline += to_str(begin - offset)
+
+            # Add the arrow
+            newline += " --> "
+
+            # Add end of subtitle
+            end_str = line[line.rfind(' ') + 1:]
+            end = datetime.strptime(end_str, "%H:%M:%S,%f")
+            newline += to_str(end - offset)
+
+            out.append(newline)
+        elif (len(line) > 0 and is_num(line)):      # Index
+            out.append(str(int(line) - offset_s))
+        else:                                       # Subtitles
+            out.append(line)
+
+# Write output in .second file
+with open(sys.argv[1] + ".second", "w") as ofile:
+    for line in out:
+        ofile.write(str(line) + '\n')
